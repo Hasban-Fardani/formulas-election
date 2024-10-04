4c27bac494f812e2d86da3edd415d7949c756875
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-10-04 22:12:12 +0700

    ref: small refactoring code

--- app/Http/Controllers/User/VotingController.php
+++ app/Http/Controllers/User/VotingController.php
@@ -36,45 +36,6 @@ class VotingController extends Controller
         return redirect()->route('user.home');
     }
 
-    public function results(Election $election)
-    {
-        $election->loadCount('votes');
-
-        $candidates = $election->candidates()->get();
-        $data = $candidates->map(function ($candidate) {
-            return [
-                'label' => $candidate->name,
-                'value' => $candidate->votes()->count()
-            ];
-        });
-    
-        $chartData = [
-            'labels' => $data->pluck('label')->toArray(),
-            'datasets' => [
-                [
-                    'label' => 'Jumlah Suara',
-                    'data' => $data->pluck('value')->toArray(),
-                    'backgroundColor' => [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    'borderColor' => [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    'borderWidth' => 1
-                ]
-            ]
-        ];
-    
-        return view('user.election.results', compact('election', 'chartData'));
-    }
+    // Deprecated. Now only admin can see the result
+    // public function results(Election $election)
 }
--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-
-Route::redirect('/', '/login', 301);
+use App\Http\Controllers\User;
+use App\Http\Controllers\Admin;
+use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
-Route::middleware('auth')->group(function () {
+Route::redirect('/', '/login', 301);
 
+Route::middleware('auth')->group(function () {
     // User Routes
     Route::name('user.')->group(function () {
-        Route::get('/home', App\Http\Controllers\User\HomeController::class)
+        Route::get('/home', User\HomeController::class)
             ->name('home');
-        Route::get('election/{election}', [App\Http\Controllers\User\VotingController::class, 'show'])
+        Route::get('election/{election}', [User\VotingController::class, 'show'])
             ->name('election.show');
-        // Route::get('election/{election}/results', [App\Http\Controllers\User\VotingController::class, 'results'])
-        //     ->name('election.results');  # deprecated
-        Route::post('vote/{candidate}', [App\Http\Controllers\User\VotingController::class, 'vote'])
+        Route::post('vote/{candidate}', [User\VotingController::class, 'vote'])
             ->name('vote');
     });
 
     // Admin Routes
     Route::middleware('can:admin')->prefix('/admin')->name('admin.')->group(function () {
-        Route::get('/', App\Http\Controllers\Admin\DasboardController::class)
+        Route::get('/', Admin\DasboardController::class)
             ->name('dashboard');
-        Route::resource('election', App\Http\Controllers\Admin\ElectionController::class)
+        Route::resource('election', Admin\ElectionController::class)
             ->except(['create']);
-        Route::resource('user', App\Http\Controllers\Admin\UserController::class);
-        Route::resource('candidate', App\Http\Controllers\Admin\CandidateController::class)
+        Route::resource('user', Admin\UserController::class);
+        Route::resource('candidate', Admin\CandidateController::class)
             ->only(['store', 'update', 'destroy']);
     });
 });
